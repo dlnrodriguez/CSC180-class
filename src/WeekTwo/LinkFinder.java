d7b04937067704761c85f198b5af73fc3d41f35b
dlnrodriguez <dylonrodriguez.phd@gmail.com>
2016-06-27 23:32:15 -0600

    first commit

---
+++ src/WeekTwo/LinkFinder.java
@@ -0,0 +1,32 @@
+package WeekTwo;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by DLN on 6/27/16.
+ */
+public class LinkFinder {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (; ; ) {
+            String input = scanner.nextLine();
+            if (input.equalsIgnoreCase("exit")) {
+                System.exit('E');
+            } else if (isXXNotXXX(input)) {
+                System.out.println("Does match.");
+            } else {
+                System.out.println("Does not match.");
+            }
+        }
+    }
+
+    public static boolean isXXNotXXX(String s) {
+        Pattern p = Pattern.compile("XX[^X]XX");
+        Matcher m = p.matcher(s);
+        boolean matches = m.matches();
+        return matches;
+    }
+}
---
+++ src/WeekTwo/SomethingFinder.java
@@ -0,0 +1,18 @@
+package WeekTwo;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Created by DLN on 6/27/16.
+ */
+public class SomethingFinder {
+    public boolean isStartXXOrEndYY(String s) {
+        Pattern p = Pattern.compile("(XX.+)|(.+YY)");
+        Matcher m = p.matcher(s);
+
+        boolean matches = m.matches();
+
+        return matches;
+    }
+}
