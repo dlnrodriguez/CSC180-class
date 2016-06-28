76c11871ae25481d162dc92da0518ea4287fc922
dlnrodriguez <dylonrodriguez.phd@gmail.com>
2016-06-28 00:49:27 -0600

    test added

---
+++ src/WeekTwo/LinkFinderTest.groovy
@@ -0,0 +1,17 @@
+package WeekTwo
+
+import org.junit.Test
+
+/**
+ * Created by DLN on 6/27/16.
+ */
+class LinkFinderTest extends GroovyTestCase {
+    void testName() {
+        assertFalse(LinkFinder.isXXNotXXX("XXXXX"))
+    }
+
+    @Test
+    public void test() {
+
+    }
+}
