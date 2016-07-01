942a66aa143820c68d02b68d0f348fa382e5031e
dlnrodriguez <dylonrodriguez.phd@gmail.com>
2016-06-30 23:25:19 -0600

    information added to LinkFinder

--- src/WeekTwo/LinkFinder.java
+++ src/WeekTwo/LinkFinder.java
@@ -9,7 +9,38 @@ import java.util.regex.Pattern;
 
 /**
  * Created by DLN on 6/28/16.
+ *
+ * For this exercise you will create class that will find links within a single web page.
+
+ Step 1. Set up your GitHub account.
+
+ http://eclipsesource.com/blogs/tutorials/egit-tutorial/ (Links to an external site.)
+
+ Step 2. Write a stub a class that has the following interface:
+
+ class LinkFinder {
+
+ public void processPage(InputStream in) {
+
+ }
+
+ public Iterator<String> getLinks() {
+
+ }
+
+ }
+
+ Step 3. On the LMS you will find an HTML page and a results file for exercise 1. Download these files.
+
+ Step 4. Write a JUnit test case for the class based on your review of the HTML file. The test case should find all the links in the HTML file and compare the findings to the results file. Be sure to commit your work.
+
+ This tutorial may be helpful: http://www.tutorialspoint.com/java/java_regular_expressions.htm (Links to an external site.)
+
+ Step 5. Using Java’s regular expression capabilities, fill out the class so that the class finds the links in the webpage. The class should store these links in a container of strings. Be sure to commit your work.
+
+ Step 6. Review your work with the instructor (including your Git log).
  */
+
 public class LinkFinder {
     private ArrayList<String> arrayLinks = new ArrayList<>();
     private Iterator<String> links;
