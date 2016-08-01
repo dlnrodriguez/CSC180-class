c4d4a34cafbde8efea36545a6c776601fe098242
dlnrodriguez <dylonrodriguez.phd@gmail.com>
2016-08-01 01:20:28 -0600

    progress

--- src/WeekThree/WebLinkFinder.java
+++ src/WeekThree/WebLinkFinder.java
@@ -8,14 +8,13 @@ import java.util.ArrayList;
  * Created by DLN on 7/25/16.
  */
 class WebLinkFinder {
-    private boolean hasLink = false;
     private LinkIterator iterator;
     private ArrayList<String> toVisit, hasVisited;
-    private int max = 20;
+    private int max = 50;
     private String startingUrl;
 
     WebLinkFinder() {
-        this.toVisit = new ArrayList<>(max);
+        this.toVisit = new ArrayList<>();
         this.hasVisited = new ArrayList<>(max);
         this.startingUrl = "http://shalladay-iis1.student.neumont.edu";
         try {
@@ -24,38 +23,46 @@ class WebLinkFinder {
         } catch (IOException e) {
             System.err.println(e.getMessage());
         }
+        print();
     }
 
     private void findLinks() throws IOException {
         while (this.iterator.hasNext() && max > 0) {
-            if (check(this.toVisit, this.iterator.next())) {
-                check(this.toVisit, this.iterator.next());
+            if (this.toVisit.size() > 0
+                    && !this.toVisit.get(0).isEmpty()) {
+                this.hasVisited.add(this.toVisit.get(0));
+                this.toVisit.remove(0);
             }
+            if (check(this.toVisit, this.iterator.next()))
+                check(this.toVisit, this.iterator.next());
         }
-        while (!this.iterator.hasNext() && max > 0) {
+        while (!this.iterator.hasNext()) {
             visit();
         }
     }
 
     private boolean check(ArrayList<String> it, String st) {
-        if (it.iterator().hasNext())
+        if (it.iterator().hasNext()) {
             for (String s : it)
                 if (s.equalsIgnoreCase(st))
                     return false;
+            for (String s : this.hasVisited)
+                if (s.equalsIgnoreCase(st))
+                    return false;
+        }
         max--;
         return it.add(st);
     }
 
     private void visit() throws IOException {
-        for (String s : this.toVisit) {
-            this.iterator = new LinkIterator(new URL(this.startingUrl + s));
-            if (this.iterator.hasNext()) findLinks();
-            else this.hasVisited.add(s);
-        }
+        String s = this.toVisit.get(0);
+        this.iterator = new LinkIterator(new URL(this.startingUrl + s));
+        findLinks();
     }
 
-    private void print() {
-        this.toVisit.forEach(System.out::println);
+    void print() {
+        this.hasVisited.forEach(System.out::println);
         System.out.println(max);
+        this.toVisit.forEach(System.out::println);
     }
 }
